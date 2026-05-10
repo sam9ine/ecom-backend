a2c301b060107027da2165fbe688bee7bdeef474
sameer <sameerr1105@gmail.com>
2026-05-10 23:26:18 +0530

    service layer started

---
+++ backend/src/main/java/com/ecom/backend/service/ProductService.java
@@ -0,0 +1,13 @@
+package com.ecom.backend.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    public String getDetailsOfProduct(){
+
+        return "product available";
+    }
+}
